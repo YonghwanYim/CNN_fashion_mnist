037d21e422a49f3176ccf0201268574d2acee123
Yonghwan Yim <uko99153@naver.com>
2018-12-16 14:16:18 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,33 +1,26 @@
 # coding: utf-8
 import numpy as np
 
-
 def identity_function(x):
     return x
-
 
 def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
-
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))    
-
 
 def sigmoid_grad(x):
     return (1.0 - sigmoid(x)) * sigmoid(x)
     
-
 def relu(x):
     return np.maximum(0, x)
-
 
 def relu_grad(x):
     grad = np.zeros(x)
     grad[x>=0] = 1
     return grad
     
-
 def softmax(x):
     if x.ndim == 2:
         x = x.T
@@ -38,17 +31,14 @@ def softmax(x):
     x = x - np.max(x) # オーバーフロー対策
     return np.exp(x) / np.sum(np.exp(x))
 
-
 def mean_squared_error(y, t):
     return 0.5 * np.sum((y-t)**2)
-
 
 def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
         
-    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
     if t.size == y.size:
         t = t.argmax(axis=1)
              
